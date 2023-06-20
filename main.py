fb877e9de19f24dd2cc4642535e48743e86c92bd
Shaahin Chaichi <shaahin.chaichi@gmail.com>
2023-06-20 20:33:58 +0330

    Post request completed

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import requests
 
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": "87Vu[P13z/hJXJ\z",
+    "username": "shaahin",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
